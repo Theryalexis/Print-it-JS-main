d5b8d40972928f38485be0d5c98a2dae0c59d2b6
Theryalexis <alexis62330@hotmail.fr>
2023-09-21 18:23:28 +0200

    slide

--- script/script.js
+++ script/script.js
@@ -19,12 +19,50 @@ const slides = [
 
 document.body.onload = function () {
 	
-	const slidegauche = document.getElementById('slidegauche')
-	const slidedroit = document.getElementById('slidedroit')
+	const slidegauche = document.getElementById('arrow_left');
+	const slidedroit = document.getElementById('arrow_right');
+	container = document.getElementById("banner");
 	
-	slidegauche.addEventListener = ("click", function())
-	slidedroit.addEventListener = ("click", function())
+	slidegauche.addEventListener("click", clickgauche);
+	slidedroit.addEventListener("click", clickdroit);
 
+	function clickgauche (){
+	position -=1;
+	if (position < 0) {
+		position = nbrSlides - 1;
+	}
+	slideshow();
+	}
+	function clickdroit (){
+	position +=1;
+	if (position >= nbrSlides) {
+		position = 0;
+	}
+	slideshow ();
+	}
+
+	let position=0;
+
+	function slideshow (){
 	
+		const imageSlide = document.querySelector(".banner-img");
+		const tagLine = document.querySelector("#banner p");
+		imageSlide.setAttribute ("src","./assets/images/slideshow/" + slides[position].image);
+		tagLine.innerHTML=slides[position].tagLine;
+		const dotSelected = document.querySelector(".dot_selected");
+		dotSelected.classList.remove('dot_selected');
+		const updateDotSelected = document.querySelector(".dots :nth-child("+(position+1)+")");
+		updateDotSelected.classList.add("dot_selected");
+	}
 
+	let elementDots = document.querySelector(".dots"); 
+	let nbrSlides = slides.length
+		for (let i=0; i<nbrSlides; i++) { 
+			let newDot = document.createElement("div");
+			elementDots.appendChild(newDot);
+			newDot.classList.add('dot');
+			if (i===position) {
+				newDot.classList.add('dot_selected');
+			}	
+		}
 }
